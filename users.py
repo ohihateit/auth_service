0b3aa07e40a552d2d6a6e481f8d567f0319583b0
whoamins <40am1ns@protonmail.com>
2021-12-27 18:34:49 +0300

    feat: Users in separate file for recursive import prevention

---
+++ users.py
@@ -0,0 +1,12 @@
+users = {
+    'test@user.com': {
+        'name': 'Alex Doe',
+        'password': '2bb9a0606d7b1f678e0657edb785898f0c4c048afc1bbbca9912ab4006806692',
+        'balance': 100_000
+    },
+    'testik@user.com': {
+        'name': 'John Doe',
+        'password': '8e16748b212e2ec3a35359207c7ba72325e9cbf83959ef8b5ef83da432bbc2e6',
+        'balance': 200_000
+    }
+}
